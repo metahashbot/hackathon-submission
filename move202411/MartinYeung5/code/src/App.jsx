473ac9c6de89a8e236ce67fb38866a0e59126990
uvd <386180127@qq.com>
2024-12-16 16:49:56 +0800

    feat: init

--- move202411/MartinYeung5/code/src/App.jsx
+++ move202411/MartinYeung5/code/src/App.jsx
@@ -1,12 +1,12 @@
-import ButtonGradient from "./assets/svg/ButtonGradient.jsx";
-import Benefits from "./components/Benefits.jsx";
-import Collaboration from "./components/Collaboration.jsx";
-import Footer from "./components/Footer.jsx";
-import Header from "./components/Header.jsx";
-import Hero from "./components/Hero.jsx";
-import Pricing from "./components/Pricing.jsx";
-import Roadmap from "./components/Roadmap.jsx";
-import Services from "./components/Services.jsx";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
+import Collaboration from "./components/Collaboration";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Pricing from "./components/Pricing";
+import Roadmap from "./components/Roadmap";
+import Services from "./components/Services";
 
 const App = () => {
   return (
